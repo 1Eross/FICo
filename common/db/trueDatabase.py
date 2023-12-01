a351bf76312614536b89de662cfda89eb0c03bd0
perfectoMIA <120304965+perfectoMIA@users.noreply.github.com>
2023-12-01 22:53:40 +0900

    я хз чё тут писать

--- common/db/trueDatabase.py
+++ common/db/trueDatabase.py
@@ -1,10 +1,17 @@
 import psycopg2
 try:
     # пытаемся подключиться к базе данных
-    connection = psycopg2.connect(dbname='FICo', user='postgres', password='#M135246i#', host='localhost') #не трогайте пж мой пароль и название моей дб, если будете менять на свою то мои данные сюда закоментите. 
+    connection = psycopg2.connect(dbname='TestDB', user='postgres', password='#M135246i#', host='localhost') #не трогайте пж мой пароль и название моей дб, если будете менять на свою то мои данные сюда закоментите. 
     cursor = connection.cursor()
     cursor.execute("SELECT version();")
     print(f"Server version: {cursor.fetchone()}") 
+    cursor.execute("select * from data_user;") #название бд в 4 и 8 строчке поменять 
+    result = cursor.fetchall()
+    print(result)
+    
+    
+    
+    cursor.close()
     
 except Exception as _ex:
     print("[INFO] Error while working with PostgreSQL", _ex)
