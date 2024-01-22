dae5eb195c9deb9f330250301451e68a4f4a5b54
gamercash65 <120200023+gamercash65@users.noreply.github.com>
2024-01-22 19:07:34 +1000

    теперь точно профиль

--- client/app/components/screens/profile/Profile.tsx
+++ client/app/components/screens/profile/Profile.tsx
@@ -1,26 +1,53 @@
-// Profile.tsx
-
-import React, { FC } from 'react';
-import { View, Text } from 'react-native';
-import Layout from '@/components/ui/layout/Layout';
-import Button from '@/components/ui/layout/bottom-menu/Button';
-import { useAuth } from '@/hooks/useAuth';
-import resources from '../../../../i18nResources';
+import { FC } from 'react'
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { Control, Controller, useForm } from 'react-hook-form'
+import Layout from '@/components/ui/layout/Layout'
+import Button from '@/components/ui/layout/bottom-menu/Button'
+import { useAuth } from '@/hooks/useAuth'
+import ProfileFields from './ProfileFields'
+import { IProfile } from '@/types/profile.interface'
 
 const Profile: FC = () => {
-    const { setUser } = useAuth();
-
-    const language = 'ru'; // Здесь установите выбранный язык
-
+    const{setUser} = useAuth() 
+    const {control} = useForm<IProfile>({
+    })
     return (
-        <Layout title={resources[language].profile}>
+        <Layout title='Профиль'>
+            <View style={styles.container}> 
+            <Image style={styles.avatar} source={require('C:\Users\MSI\OneDrive\Рабочий стол\Avatar.jpg')} />
+            <ProfileFields control={control}/>
+            </View>
+            <Button>Редактировать профиль</Button>
             <Button onPress={() => setUser(null)}>
-                <View className='items-center justify-center flex-1'>
+                <View className='items-center justify-center flex-1'> 
+
                 </View>
-                {resources[language].exit}
+                Exit
             </Button>
         </Layout>
-    );
-};
-
-export default Profile;
+    )
+    
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+    },
+    avatar: {
+      width: 120,
+      height: 120,
+      borderRadius: 60,
+      marginBottom: 16,
+    },
+    editButton: {
+      backgroundColor: '',
+      paddingVertical: 8,
+      paddingHorizontal: 16,
+      borderRadius: 8,
+    },
+    editButtonText: {
+      color: 'white',
+      fontWeight: 'bold',
+    },
+  });
+export default Profile
